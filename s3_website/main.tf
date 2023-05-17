def6eb100a905dfdaca74e2c0ed727e7a12755f4
VladislavShevchukNure <128866805+VladislavShevchukNure@users.noreply.github.com>
2023-05-17 12:05:03 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -39,38 +39,46 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
 		key = "error.html"
 	}
 }
-	#policy = data.aws_iam_policy_document.allow_access.json
-	#arn:aws:s3:::terraform-bucket-shevchuk3/*
-resource "aws_s3_bucket_policy" "allow_access" {
-	bucket = aws_s3_bucket.website.id
-	policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-	  "Principal": "*",
-      "Action": [ "s3:*" ],
-      "Resource": [
-        "${aws_s3_bucket.website.arn}",
-        "${aws_s3_bucket.website.arn}/*"
-      ]
-    }
-  ]
-}
-EOF
+
+#resource "aws_s3_bucket_policy" "allow_access" {
+#	bucket = aws_s3_bucket.website.id
+#	policy = data.aws_iam_policy_document.allow_access.json
+#}
+#
+#data "aws_iam_policy_document" "allow_access" {
+#	statement {
+#		principals {
+#			type = "AWS"
+#			identifiers = ["*"]
+#		}
+#		actions = [ "s3:GetObject" ]
+#		resources = [ aws_s3_bucket.website.arn,
+#		"${aws_s3_bucket.website.arn}/*", ]
+#	}
+#}
+
+resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
+  bucket = aws_s3_bucket.website.id
+  policy = data.aws_iam_policy_document.allow_access_from_another_account.json
 }
 
-data "aws_iam_policy_document" "allow_access" {
-	statement {
-		principals {
-			type = "AWS"
-			identifiers = ["*"]
-		}
-		actions = [ "s3:GetObject" ]
-		resources = [ aws_s3_bucket.website.arn,
-		"${aws_s3_bucket.website.arn}/*", ]
-	}
+data "aws_iam_policy_document" "allow_access_from_another_account" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      aws_s3_bucket.website.arn,
+      "${aws_s3_bucket.website.arn}/*",
+    ]
+  }
 }
 
 resource "aws_s3_object" "indexfile" {
